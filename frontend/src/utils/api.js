a3fd17ba0e3e6ac6c69f9f567a58cb27f345288f
Aizada <aizada.omarhan01@gmail.com>
2025-09-13 14:58:53 +0500

    Fix API baseURL with /api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
-  withCredentials: true, // для cookie авторизации
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api", // 👈 ОБЯЗАТЕЛЬНО "/api"
+  withCredentials: true,
 });
 
 export default API;
+
 
